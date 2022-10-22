ebe9eaf9eea0231f1566ed1f129b44c4ddb4f8f3
hsieh1093359 <100330536+hsieh1093359@users.noreply.github.com>
2022-10-22 15:39:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -86,8 +86,8 @@ def main():
         "Real time audio filter (sendrecv)": app_audio_filter,
         "Delayed echo (sendrecv)": app_delayed_echo,
         "Consuming media files on server-side and streaming it to browser (recvonly)": app_streaming,  # noqa: E501
-        "WebRTC is sendonly and images are shown via st.image() (sendonly)": app_sendonly_video,  # noqa: E501
-        "WebRTC is sendonly and audio frames are visualized with matplotlib (sendonly)": app_sendonly_audio,  # noqa: E501
+        #"WebRTC is sendonly and images are shown via st.image() (sendonly)": app_sendonly_video,  # noqa: E501
+        #"WebRTC is sendonly and audio frames are visualized with matplotlib (sendonly)": app_sendonly_audio,  # noqa: E501
         "Simple video and audio loopback (sendrecv)": app_loopback,
         "Configure media constraints and HTML element styles with loopback (sendrecv)": app_media_constraints,  # noqa: E501
         "Control the playing state programatically": app_programatically_play,
@@ -501,110 +501,110 @@ def app_streaming():
     )
 
 
-def app_sendonly_video():
-    """A sample to use WebRTC in sendonly mode to transfer frames
-    from the browser to the server and to render frames via `st.image`."""
-    webrtc_ctx = webrtc_streamer(
-        key="video-sendonly",
-        mode=WebRtcMode.SENDONLY,
-        rtc_configuration=RTC_CONFIGURATION,
-        media_stream_constraints={"video": True},
-    )
+# def app_sendonly_video():
+#     """A sample to use WebRTC in sendonly mode to transfer frames
+#     from the browser to the server and to render frames via `st.image`."""
+#     webrtc_ctx = webrtc_streamer(
+#         key="video-sendonly",
+#         mode=WebRtcMode.SENDONLY,
+#         rtc_configuration=RTC_CONFIGURATION,
+#         media_stream_constraints={"video": True},
+#     )
 
-    image_place = st.empty()
+#     image_place = st.empty()
 
-    while True:
-        if webrtc_ctx.video_receiver:
-            try:
-                video_frame = webrtc_ctx.video_receiver.get_frame(timeout=1)
-            except queue.Empty:
-                logger.warning("Queue is empty. Abort.")
-                break
+#     while True:
+#         if webrtc_ctx.video_receiver:
+#             try:
+#                 video_frame = webrtc_ctx.video_receiver.get_frame(timeout=1)
+#             except queue.Empty:
+#                 logger.warning("Queue is empty. Abort.")
+#                 break
 
-            img_rgb = video_frame.to_ndarray(format="rgb24")
-            image_place.image(img_rgb)
-        else:
-            logger.warning("AudioReciver is not set. Abort.")
-            break
+#             img_rgb = video_frame.to_ndarray(format="rgb24")
+#             image_place.image(img_rgb)
+#         else:
+#             logger.warning("AudioReciver is not set. Abort.")
+#             break
 
 
-def app_sendonly_audio():
-    """A sample to use WebRTC in sendonly mode to transfer audio frames
-    from the browser to the server and visualize them with matplotlib
-    and `st.pyplot`."""
-    webrtc_ctx = webrtc_streamer(
-        key="sendonly-audio",
-        mode=WebRtcMode.SENDONLY,
-        audio_receiver_size=256,
-        rtc_configuration=RTC_CONFIGURATION,
-        media_stream_constraints={"audio": True},
-    )
+#def app_sendonly_audio():
+    # """A sample to use WebRTC in sendonly mode to transfer audio frames
+    # from the browser to the server and visualize them with matplotlib
+    # and `st.pyplot`."""
+    # webrtc_ctx = webrtc_streamer(
+    #     key="sendonly-audio",
+    #     mode=WebRtcMode.SENDONLY,
+    #     audio_receiver_size=256,
+    #     rtc_configuration=RTC_CONFIGURATION,
+    #     media_stream_constraints={"audio": True},
+    # )
 
-    fig_place = st.empty()
+    # fig_place = st.empty()
 
-    fig, [ax_time, ax_freq] = plt.subplots(
-        2, 1, gridspec_kw={"top": 1.5, "bottom": 0.2}
-    )
+    # fig, [ax_time, ax_freq] = plt.subplots(
+    #     2, 1, gridspec_kw={"top": 1.5, "bottom": 0.2}
+    # )
 
-    sound_window_len = 5000  # 5s
-    sound_window_buffer = None
-    while True:
-        if webrtc_ctx.audio_receiver:
-            try:
-                audio_frames = webrtc_ctx.audio_receiver.get_frames(timeout=1)
-            except queue.Empty:
-                logger.warning("Queue is empty. Abort.")
-                break
+    # sound_window_len = 5000  # 5s
+    # sound_window_buffer = None
+    # while True:
+    #     if webrtc_ctx.audio_receiver:
+    #         try:
+    #             audio_frames = webrtc_ctx.audio_receiver.get_frames(timeout=1)
+    #         except queue.Empty:
+    #             logger.warning("Queue is empty. Abort.")
+    #             break
 
-            sound_chunk = pydub.AudioSegment.empty()
-            for audio_frame in audio_frames:
-                sound = pydub.AudioSegment(
-                    data=audio_frame.to_ndarray().tobytes(),
-                    sample_width=audio_frame.format.bytes,
-                    frame_rate=audio_frame.sample_rate,
-                    channels=len(audio_frame.layout.channels),
-                )
-                sound_chunk += sound
+    #         sound_chunk = pydub.AudioSegment.empty()
+    #         for audio_frame in audio_frames:
+    #             sound = pydub.AudioSegment(
+    #                 data=audio_frame.to_ndarray().tobytes(),
+    #                 sample_width=audio_frame.format.bytes,
+    #                 frame_rate=audio_frame.sample_rate,
+    #                 channels=len(audio_frame.layout.channels),
+    #             )
+    #             sound_chunk += sound
 
-            if len(sound_chunk) > 0:
-                if sound_window_buffer is None:
-                    sound_window_buffer = pydub.AudioSegment.silent(
-                        duration=sound_window_len
-                    )
+    #         if len(sound_chunk) > 0:
+    #             if sound_window_buffer is None:
+    #                 sound_window_buffer = pydub.AudioSegment.silent(
+    #                     duration=sound_window_len
+    #                 )
 
-                sound_window_buffer += sound_chunk
-                if len(sound_window_buffer) > sound_window_len:
-                    sound_window_buffer = sound_window_buffer[-sound_window_len:]
+    #             sound_window_buffer += sound_chunk
+    #             if len(sound_window_buffer) > sound_window_len:
+    #                 sound_window_buffer = sound_window_buffer[-sound_window_len:]
 
-            if sound_window_buffer:
-                # Ref: https://own-search-and-study.xyz/2017/10/27/python%E3%82%92%E4%BD%BF%E3%81%A3%E3%81%A6%E9%9F%B3%E5%A3%B0%E3%83%87%E3%83%BC%E3%82%BF%E3%81%8B%E3%82%89%E3%82%B9%E3%83%9A%E3%82%AF%E3%83%88%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%A0%E3%82%92%E4%BD%9C/  # noqa
-                sound_window_buffer = sound_window_buffer.set_channels(
-                    1
-                )  # Stereo to mono
-                sample = np.array(sound_window_buffer.get_array_of_samples())
+    #         if sound_window_buffer:
+    #             # Ref: https://own-search-and-study.xyz/2017/10/27/python%E3%82%92%E4%BD%BF%E3%81%A3%E3%81%A6%E9%9F%B3%E5%A3%B0%E3%83%87%E3%83%BC%E3%82%BF%E3%81%8B%E3%82%89%E3%82%B9%E3%83%9A%E3%82%AF%E3%83%88%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%A0%E3%82%92%E4%BD%9C/  # noqa
+    #             sound_window_buffer = sound_window_buffer.set_channels(
+    #                 1
+    #             )  # Stereo to mono
+    #             sample = np.array(sound_window_buffer.get_array_of_samples())
 
-                ax_time.cla()
-                times = (np.arange(-len(sample), 0)) / sound_window_buffer.frame_rate
-                ax_time.plot(times, sample)
-                ax_time.set_xlabel("Time")
-                ax_time.set_ylabel("Magnitude")
+    #             ax_time.cla()
+    #             times = (np.arange(-len(sample), 0)) / sound_window_buffer.frame_rate
+    #             ax_time.plot(times, sample)
+    #             ax_time.set_xlabel("Time")
+    #             ax_time.set_ylabel("Magnitude")
 
-                spec = np.fft.fft(sample)
-                freq = np.fft.fftfreq(sample.shape[0], 1.0 / sound_chunk.frame_rate)
-                freq = freq[: int(freq.shape[0] / 2)]
-                spec = spec[: int(spec.shape[0] / 2)]
-                spec[0] = spec[0] / 2
+    #             spec = np.fft.fft(sample)
+    #             freq = np.fft.fftfreq(sample.shape[0], 1.0 / sound_chunk.frame_rate)
+    #             freq = freq[: int(freq.shape[0] / 2)]
+    #             spec = spec[: int(spec.shape[0] / 2)]
+    #             spec[0] = spec[0] / 2
 
-                ax_freq.cla()
-                ax_freq.plot(freq, np.abs(spec))
-                ax_freq.set_xlabel("Frequency")
-                ax_freq.set_yscale("log")
-                ax_freq.set_ylabel("Magnitude")
+    #             ax_freq.cla()
+    #             ax_freq.plot(freq, np.abs(spec))
+    #             ax_freq.set_xlabel("Frequency")
+    #             ax_freq.set_yscale("log")
+    #             ax_freq.set_ylabel("Magnitude")
 
-                fig_place.pyplot(fig)
-        else:
-            logger.warning("AudioReciver is not set. Abort.")
-            break
+    #             fig_place.pyplot(fig)
+    #     else:
+    #         logger.warning("AudioReciver is not set. Abort.")
+    #         break
 
 
 def app_media_constraints():
